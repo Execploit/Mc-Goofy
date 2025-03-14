b1651bafb5e1340270f2ef9b723513b32bcf0b90
Execploit <exec127.0.0.1@gmail.com>
2025-03-14 18:50:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,32 +1,30 @@
-
-
 module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://McGoofy:McGoofy@discordbot.yzg54.mongodb.net/?retryWrites=true&w=majority",
+  mongodbUri: "mongodb+srv://McGoofy:McGoofy@discordbot.yzg54.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityType: "LISTENING",  // Available activity types: LISTENING, PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
-   "lavalink": {
-        "identifier": "GlaceYT",
-        "password": "enteryourcustompass",
-        "host": "180.188.226.76",
-        "port":  7019,
-        "secure": false
-      }
-  {
-       "identifier": "GlaceYT",
-        "password": "enteryourcustompass",
-        "host": "180.188.226.76",
-        "port":  7019,
-        "secure": false
+    {
+      identifier: "GlaceYT",
+      password: "enteryourcustompass",
+      host: "180.188.226.76",
+      port: 7019,
+      secure: false
+    },
+    {
+      identifier: "GlaceYT",
+      password: "enteryourcustompass",
+      host: "180.188.226.76",
+      port: 7019,
+      secure: false
     }
   ]
 }
